a0b958f11b5153db3f443d2fa5ae07632cf82d7f
DDQuin <ddquin420@gmail.com>
2022-06-19 19:36:03 +0100

    ex 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,25 +4,36 @@
 
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
   // eslint-disable-next-line prefer-destructuring
   const body = request.body;
+  const users = await User.find({});
+  const user = users[0];
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
+    // eslint-disable-next-line no-underscore-dangle
+    user: user._id,
   });
+
   if (!body.title || !body.url) {
     response.status(400).end();
   } else {
     const savedBlog = await blog.save();
+    // eslint-disable-next-line no-underscore-dangle
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
     response.status(201).json(savedBlog);
   }
 });
