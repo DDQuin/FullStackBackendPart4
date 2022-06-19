0ef7b334ed0c0058066f17b57aaa214946bf41a4
DDQuin <ddquin420@gmail.com>
2022-06-19 20:19:34 +0100

    ex 4.18

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 require('express-async-errors');
 
 const app = express();
@@ -30,6 +31,7 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
