719b84441a2d3163befdcf6ddd20e746aef151cd
DDQuin <ddquin420@gmail.com>
2022-06-19 19:18:20 +0100

    ex 4.16

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -195,6 +195,47 @@ describe('when there is initially one user in db', () => {
     const usersAtEnd = await helper.usersInDb();
     expect(usersAtEnd).toEqual(usersAtStart);
   });
+
+  test('creation fails with proper statuscode and message if username not given', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      name: 'Superuser',
+      password: 'salainen',
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(result.body.error).toContain('username and password must be given');
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toEqual(usersAtStart);
+  });
+
+  test('creation fails with proper statuscode and message if password given with < 3 chars', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'test',
+      name: 'Superuser',
+      password: 'sa',
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(result.body.error).toContain('username and password must have atleast 3 characters');
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toEqual(usersAtStart);
+  });
 });
 
 afterAll(() => {
