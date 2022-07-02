a202c3120123a8aec0389a51688bc4a4f278fa18
DDQuin <ddquin420@gmail.com>
2022-07-02 21:08:53 +0100

    ex 7.18 comments addded to backend

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,18 +2,31 @@
 /* eslint-disable consistent-return */
 /* eslint-disable no-console */
 
-const blogsRouter = require('express').Router();
-const jwt = require('jsonwebtoken');
-const Blog = require('../models/blog');
-const User = require('../models/user');
+const blogsRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
+const Blog = require("../models/blog");
+const User = require("../models/user");
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({}).populate('user', { username: 1, name: 1 });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post("/:id/comments", async (request, response, next) => {
+  const blog = await Blog.findById(request.params.id);
+  if (!blog) {
+    return response.status(400).json({ error: "blog not found" }).end();
+  }
+  const body = request.body;
+  if (!body.comment) {
+    return response.status(400).json({ error: "missing comment" }).end();
+  }
+  blog.comments = blog.comments.concat(body.comment);
+  await blog.save();
+  response.status(201).json(blog);
+});
+
+blogsRouter.post("/", async (request, response, next) => {
   // eslint-disable-next-line prefer-destructuring
   const body = request.body;
   /*
@@ -38,7 +51,7 @@ blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog({ ...request.body, user: user.id });
 
   if (!body.title || !body.url) {
-    return response.status(400).json({ error: 'missing title or url' }).end();
+    return response.status(400).json({ error: "missing title or url" }).end();
   }
 
   const savedBlog = await blog.save();
@@ -49,7 +62,7 @@ blogsRouter.post('/', async (request, response, next) => {
   response.status(201).json(savedBlog);
 });
 
-blogsRouter.delete('/:id', async (request, response, next) => {
+blogsRouter.delete("/:id", async (request, response, next) => {
   const blog = await Blog.findById(request.params.id);
   let decodedToken = null;
   try {
@@ -58,18 +71,18 @@ blogsRouter.delete('/:id', async (request, response, next) => {
     next(exception);
   }
   if (!decodedToken || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' });
+    return response.status(401).json({ error: "token missing or invalid" });
   }
 
   if (blog.user.toString() === decodedToken.id) {
     await Blog.findByIdAndRemove(request.params.id);
     response.status(204).end();
   } else {
-    response.status(401).json({ error: 'wrong user' });
+    response.status(401).json({ error: "wrong user" });
   }
 });
 
-blogsRouter.put('/:id', async (request, response, next) => {
+blogsRouter.put("/:id", async (request, response, next) => {
   // eslint-disable-next-line prefer-destructuring
   const body = request.body;
 
@@ -80,7 +93,7 @@ blogsRouter.put('/:id', async (request, response, next) => {
     next(exception);
   }
   if (!decodedToken || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' });
+    return response.status(401).json({ error: "token missing or invalid" });
   }
 
   const blog = {
@@ -90,7 +103,9 @@ blogsRouter.put('/:id', async (request, response, next) => {
     likes: body.likes,
   };
 
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
   response.json(updatedBlog);
 });
 
